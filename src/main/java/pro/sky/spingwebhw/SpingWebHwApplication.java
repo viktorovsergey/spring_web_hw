4a039ec33c92dd085edeec7fc1de9527b695afda
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-04-14 20:04:37 +0300

    initial commit

---
+++ src/main/java/pro/sky/spingwebhw/SpingWebHwApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.spingwebhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingWebHwApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpingWebHwApplication.class, args);
+    }
+
+}
